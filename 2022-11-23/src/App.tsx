58eb249f55fd1dd9d8958d99b9bf6417bdcb363f
bolawen <335303383@qq.com>
2023-01-12 09:42:32 +0800

    推送

--- 2022-11-23/src/App.tsx
+++ 2022-11-23/src/App.tsx
@@ -1,18 +1,22 @@
 import React, {useState} from 'react';
 import logo from '@/assets/svg/logo.svg';
-import Select, {Option} from 'rc-select';
+
+interface ChildPropsType {
+    setTitle: (value: string) => void;
+}
+
+function Child(props: ChildPropsType) {
+    const {setTitle} = props;
+
+    return <div>Child</div>;
+}
 
 function App() {
     const [title, setTitle] = useState<string>('Vite-React 模板');
-    const name: string = 'st';
-    const age: number = 200;
     return (
         <div>
-            <Select>
-                <Option value="jack">jack</Option>
-                <Option value="lucy">lucy</Option>
-                <Option value="yiminghe">yiminghe</Option>
-            </Select>
+            App
+            <Child setTitle={setTitle} />
         </div>
     );
 }
