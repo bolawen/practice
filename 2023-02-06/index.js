3861c09ca39f8c40a5a97688d38c1cdf68f0681e
Wenqiang Zhang <zhangwenqiang@umu.com>
2023-02-06 21:10:49 +0800

    推送

--- 2023-02-06/index.js
+++ 2023-02-06/index.js
@@ -1,9 +1,49 @@
-const cloneDeep = require("lodash/cloneDeep");
+class EventEmitter{
+    static defaultMaxListeners = 10;
 
-const obj = {
-    name: "柏拉图",
-    age: 23
+    constructor(){
+        this.events = {};
+    }
+    
+    on(type,listener,flag){
+        if (!this.events) {
+            this.events = Object.create(null)
+        }
+        if(this.events[type]){
+            if(flag){
+                this.events[type].unshift(listener);
+            }else{
+                this.events[type].push(listener);
+            }
+        }else{
+            this.events[type] = [listener]
+        }
+
+        if(type !== "newListener"){
+            this.emit("newListener",type);
+        }
+    }
+    emit(type,...args){
+        if(this.events[type]){
+            this.events[type].forEach((fn)=>{
+                fn.call(this,...args);
+            });
+        }
+    }
+    once(type,listener){
+        const that = this;
+        function only(){
+            listener();
+            that.removeListener(type,only);
+        }
+        only.origin = listener;
+        that.on(type.only);
+    }
+    off(type,listener){
+        if(this.events[type]){
+            this.events[type] = this.events[type].filter(fn=>{
+                return fn !== listener && fn.origin !== listener;
+            });
+        }
+    }
 }
-const objCopy = cloneDeep(obj);
-
-console.log(objCopy === obj);
