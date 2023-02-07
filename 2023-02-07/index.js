3a2d2fe0f8b2ba41adad5ccf9e20d22db052af4a
Wenqiang Zhang <zhangwenqiang@umu.com>
2023-02-07 10:17:47 +0800

    推送

--- 2023-02-07/index.js
+++ 2023-02-07/index.js
@@ -1,6 +1,2 @@
-const value = 3.5;
-
-console.log(+value); // 3.5
-console.log(value >> 0); // 3
-console.log(value >>> 0); // 3
-console.log(Number(value)); // 3.5
+console.log(JSON.parse("true"), typeof JSON.parse("true")); // true boolean
+console.log(JSON.parse("false"), typeof JSON.parse("false")); // false boolean
