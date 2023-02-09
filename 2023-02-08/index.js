e6844c80d423edb0a77f90a2ddcf562304bcfa8f
bolawen <335303383@qq.com>
2023-02-09 08:20:12 +0800

    推送

--- 2023-02-08/index.js
+++ 2023-02-08/index.js
@@ -1,7 +1,17 @@
-function Person(){
-
+function groupValue(value, index, flag) {
+  const reg = new RegExp(`\\d{1,${index}}(?=(\\d{${index}})+$)`, "g");
+  return String(value).replace(reg, function (match, ...args) {
+    return match + flag;
+  });
 }
-Person.prototype[Symbol.toStringTag] = "Person";
-const person = new Person();
+function formatThousands(value) {
+  const strValue = String(value);
+  const cacheList = strValue.split(".");
+  const int = cacheList[0];
+  const fraction = cacheList[1];
+  const result = groupValue(int, 3, ",");
+  return result + (!!fraction ? "." + fraction : "");
+}
 
-console.log(Object.prototype.toString.call(person));
+const num = 4453232566.9888;
+console.log(formatThousands(num));
