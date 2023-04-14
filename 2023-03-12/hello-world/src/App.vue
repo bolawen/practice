bb97a2e753b3503ae754786c0f3489effb22a3a2
bolawen <335303383@qq.com>
2023-04-14 08:19:52 +0800

    推送

--- 2023-03-12/hello-world/src/App.vue
+++ 2023-03-12/hello-world/src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
+    <button v-on:click="event">点击</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  data(){
+  data() {
     return {
-    }
-  }
+      event: "handleClick"
+    };
+  },
+  methods: {
+    handleClick($event) {
+      console.log($event);
+    },
+  },
 };
 </script>
