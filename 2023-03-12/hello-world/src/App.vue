6ef48f3ef4c0ca7be07ea8b1411c36d196a317fc
bolawen <335303383@qq.com>
2023-04-21 08:32:30 +0800

    推送

--- 2023-03-12/hello-world/src/App.vue
+++ 2023-03-12/hello-world/src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <div></div>
+  <div>
+    <HelloWorld></HelloWorld>
+    <button @click="addNum">App 组件 增加 State.Num</button>
+    <button @click="addListItem">App 组件 增加 State.List</button>
+  </div>
 </template>
 
 <script>
+import HelloWorld from "./HelloWorld.vue";
+
 export default {
   name: "App",
+  components: {
+    HelloWorld,
+  },
   data() {
-    return {};
+    return {
+      state: {
+        num: 0,
+        list: [],
+      },
+    };
   },
-  mounted() {
+  provide() {
+    return {
+      state: this.state,
+      addNum: this.addNum,
+      addListItem: this.addListItem
+    };
   },
-  methods: {},
+  methods: {
+    addNum() {
+      this.state.num = this.state.num + 1;
+    },
+    addListItem(){
+      const item = this.state.num;
+      this.state.list.push(item)
+    }
+  },
 };
 </script>
---
+++ 2023-03-12/hello-world/src/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{ this.value.num }}
+
+    <ul>
+      <li :key="index" v-for="(item,index) in value.list">{{ item }}</li>
+    </ul>
+
+    <button @click="addListItem">HelloWorld 组件 增加 State.List</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  inject: {
+    value: {
+      from: "state",
+      default: () => {
+        return { num: 0, list: [] };
+      },
+    },
+    addNum: {
+      from: "addNum"
+    },
+    addListItem: {
+      from : "addListItem"
+    }
+  },
+};
+</script>
