ed93c9cd0651132ee1ba5f7d3b3fdc8051360f82
bolawen <zwq18235130928@163.com>
2023-05-09 08:28:58 +0800

    推送

---
+++ 2023-05-02/vue3-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <li :key="item.id" v-for="item in list" :ref="(el) => setListItemRef(el, item.id)">
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const list = ref([
+  {
+    id: 1,
+    name: '哈哈'
+  },
+  {
+    id: 2,
+    name: '嘻嘻'
+  },
+  {
+    id: 3,
+    name: '呵呵'
+  }
+])
+
+const listItemRef = ref<{ [key: number]: any }[]>([])
+
+const setListItemRef = (dom: any, id: number) => {
+  listItemRef.value.push({ [id]: dom })
+  console.log(listItemRef)
+}
+</script>
---
+++ 2023-05-02/vue3-project/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button @click="say">按钮</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['say']);
+
+const say = ()=>{
+  emit('say',"哈哈哈");
+}
+</script>
