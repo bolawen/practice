155e8fbcce16ad5d2e51bd25d3d86bd4e0100390
bolawen <zwq18235130928@163.com>
2023-05-17 08:28:30 +0800

    推送

--- 2023-05-02/vue3-project/src/App.vue
+++ 2023-05-02/vue3-project/src/App.vue
@@ -1,14 +1,44 @@
 <template>
   <div>
-    {{ store.a }}
-    {{ store.countA }}
+    <a @click="push('/')">Home</a>
+    <a @click="push('/about')">About</a>
 
-    <Child></Child>
+    <component :is="currentView" />
   </div>
 </template>
 
 <script setup lang="ts">
-import Child from './Child.vue'
-import { useGlobalState } from './stores/useGlobalState';
-const store = useGlobalState();
+import { computed, ref } from 'vue'
+import Home from './views/HomeView.vue'
+import About from './views/AboutView.vue'
+
+const routes = {
+  '/': Home,
+  '/about': About
+}
+
+const currentPath = ref(window.location.pathname)
+
+const renderView = ()=>{
+  currentPath.value = window.location.pathname;
+}
+
+const push = (path: string) => {
+  const stateObj = {
+    a: 1,
+    b: 2
+  }
+  const title = ''
+  const url = path
+  history.pushState(stateObj, title, url);
+  renderView();
+}
+
+window.addEventListener('popstate',()=>{
+  renderView();
+});
+
+const currentView = computed(() => {
+  return routes[currentPath.value as keyof typeof routes || '/']
+})
 </script>
