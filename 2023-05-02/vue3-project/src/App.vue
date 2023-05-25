703e469efbc63b79801376edb1dee4c1d24c6158
bolawen <zwq18235130928@163.com>
2023-05-25 08:23:36 +0800

    推送

--- 2023-05-02/vue3-project/src/App.vue
+++ 2023-05-02/vue3-project/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="wrap">
     <router-link to="/">首页</router-link>
     <router-link to="/about">关于</router-link>
     <router-link to="/home">Home</router-link>
@@ -7,7 +7,11 @@
 
     <button @click="jump">关于</button>
 
-    <router-view></router-view>
+    <router-view v-slot="{ Component, route }">
+      <transition :name="route.meta.transition || 'fade'">
+        <component :key="route.path" :is="Component"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -20,3 +24,53 @@ const jump = ()=>{
   router.replace({ name: 'about', params: { a: 1}});
 }
 </script>
+
+<style scoped>
+.wrap {
+  width: 400px;
+  height: 600px;
+  overflow: hidden;
+  position: relative;
+  margin-top: 24px;
+}
+
+/**
+ * @description: slide-left
+ */
+.slide-left-enter-active,
+.slide-left-leave-active {
+  transition: all 0.5s ease-out;
+}
+
+.slide-left-enter {
+  position: absolute;
+  transform: translateX(-100%);
+  opacity: 0;
+}
+
+.slide-left-leave-to {
+  position: absolute;
+  transform: translateX(100%);
+  opacity: 0;
+}
+
+/**
+ * @description: slide-right
+ */
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition: all 0.5s ease-out;
+}
+
+.slide-right-enter {
+  position: absolute;
+  transform: translateX(100%);
+  opacity: 0;
+}
+
+.slide-right-leave-to {
+  position: absolute;
+  transform: translateX(-100%);
+  opacity: 0;
+}
+</style>
--- 2023-05-02/vue3-project/src/router/index.ts
+++ 2023-05-02/vue3-project/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteLocationNormalized } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -6,11 +6,19 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView,
+    meta: {
+      icon: "",
+      auth: [],
+    }
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import('../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue'),
+    meta: {
+      icon: "",
+      auth: [],
+    }
   },
 ]
 
@@ -19,20 +27,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL)
 })
 
-function canUserAccess(to: RouteLocationNormalized){
-  return new Promise((resolve)=>{
-    setTimeout(()=>{
-      resolve(true);
-    },3000);
-  });
-}
-
-router.beforeEach(async (to,from)=>{
-  const canAccess = await canUserAccess(to);
-  if(canAccess){
-    return true;
-  }
-  return '/login'
+router.afterEach((to,from)=>{
+  const toDepth = to.path.split('/').length
+  const fromDepth = from.path.split('/').length
+  to.meta.transition = toDepth < fromDepth ? 'slide-right' : 'slide-left'
 });
+
 
 export default router
