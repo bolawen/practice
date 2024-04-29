056c450ab4c0d3f6d435df792ec195e76a8ed270
zhangwenqiang <zhangwenqiang@umu.com>
2024-04-29 18:12:29 +0800

    推送

---
+++ monitoringAndEarlyWarning/sentry/sentry-react/src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  tracePropagationTargets: ["sentry"],
+  dsn: "https://11abcb57930e503bf5003f07e720a08f@o4507168918601728.ingest.us.sentry.io/4507168922402816",
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
