b06bda19def5270b99024bc806b3e2d333eeb088
zhangwenqiang <zhangwenqiang@umu.com>
2024-08-01 20:21:55 +0800

    Webpack 搭建

---
+++ webpack/swc-loader/index.ts
@@ -0,0 +1,5 @@
+const a: number = 3;
+const b: string = "";
+
+console.log("a", a);
+console.log("b", b);
---
+++ webpack/swc-loader/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "index.ts"),
+  output: {
+    clean: true,
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: ["swc-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
