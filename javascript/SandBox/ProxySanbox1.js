6fe6cec1b3f57d4e0b093b32c056aef0ab8819d6
zhangwenqiang <zhangwenqiang@umu.com>
2024-09-05 10:16:25 +0800

    笔记微前端

--- javascript/SandBox/ProxySanbox1.js
+++ javascript/SandBox/ProxySanbox1.js
@@ -1,41 +1,71 @@
+function createFakeWindow(globalContext) {
+  const fakeWindow = {};
+
+  Object.getOwnPropertyNames(globalContext)
+    .filter((p) => {
+      const descriptor = Object.getOwnPropertyDescriptor(globalContext, p);
+      return !descriptor?.configurable;
+    })
+    .forEach((p) => {
+      const descriptor = Object.getOwnPropertyDescriptor(globalContext, p);
+      if (descriptor) {
+        const hasGetter = Object.prototype.hasOwnProperty.call(
+          descriptor,
+          "get"
+        );
+
+        if (p === "top" || p === "parent" || p === "self" || p === "window") {
+          descriptor.configurable = true;
+          if (!hasGetter) {
+            descriptor.writable = true;
+          }
+        }
+
+        Object.defineProperty(fakeWindow, p, Object.freeze(descriptor));
+      }
+    });
+
+  return fakeWindow;
+}
+
 class ProxySandbox {
-    constructor() {
-        this.fakeWindow = {};
-        this.isRunning = false;
-        this.proxy = new Proxy(this.fakeWindow, {
-            get: (target, key) => {
-                if (key === 'window' || key === 'self' || key === 'globalThis') {
-                    return this.proxy;
-                }
-                return target[key] || window[key];
-            },
-            set: (target, key, value) => {
-                if (this.isRunning) {
-                    target[key] = value;
-                    return true;
-                }
-                return false;
-            },
-            has: (target, key) => {
-                return key in target || key in window;
-            },
-            deleteProperty: (target, key) => {
-                if (this.isRunning) {
-                    delete target[key];
-                    return true;
-                }
-                return false;
-            },
-        });
-    }
+  constructor() {
+    this.fakeWindow = createFakeWindow(window);
+    this.isRunning = false;
+    this.proxy = new Proxy(this.fakeWindow, {
+      get: (target, key) => {
+        if (key === "window" || key === "self" || key === "globalThis") {
+          return this.proxy;
+        }
+        return target[key] || window[key];
+      },
+      set: (target, key, value) => {
+        if (this.isRunning) {
+          target[key] = value;
+          return true;
+        }
+        return false;
+      },
+      has: (target, key) => {
+        return key in target || key in window;
+      },
+      deleteProperty: (target, key) => {
+        if (this.isRunning) {
+          delete target[key];
+          return true;
+        }
+        return false;
+      },
+    });
+  }
 
-    active() {
-        this.isRunning = true;
-    }
+  active() {
+    this.isRunning = true;
+  }
 
-    inactive() {
-        this.isRunning = false;
-    }
+  inactive() {
+    this.isRunning = false;
+  }
 }
 
 const sandBox1 = new ProxySandbox("sandBox1");
