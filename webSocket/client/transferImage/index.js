481c4058c167b20921cc2330206ae4fbb03baad2
zhangwenqiang <zhangwenqiang@umu.com>
2024-10-17 14:36:24 +0800

    WebSocket 学习

--- webSocket/client/transferImage/index.js
+++ webSocket/client/transferImage/index.js
@@ -1,34 +1,92 @@
 const socket = new WebSocket("ws://localhost:3000");
 const inputFileEl = document.getElementById("input-file");
 
-socket.addEventListener("open", ()=>{
-    console.log("WebSocket Connection opened");
+socket.addEventListener("open", () => {
+  console.log("WebSocket Connection opened");
 });
 
-socket.addEventListener("close", ()=>{
-    console.log("WebSocket Connection closed");
+socket.addEventListener("close", () => {
+  console.log("WebSocket Connection closed");
 });
 
-socket.addEventListener("message", (event)=>{
-    console.log("WebSocket received from server:", event.data);
+socket.addEventListener("message", (event) => {
+  console.log("WebSocket received from server:", event.data);
 });
 
+async function uploadFileForFile(file) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const response = await fetch("http://localhost:3000/upload", {
+    method: "POST",
+    body: formData,
+  });
+  const data = await response.json();
+  console.log("File uploaded by file:", data);
+}
+
+async function uploadFileForBlob(blob) {
+  const formData = new FormData();
+  formData.append("blob", blob);
+
+  const response = await fetch("http://localhost:3000/upload", {
+    method: "POST",
+    body: formData,
+  });
+  const data = await response.json();
+  console.log("File uploaded by blob:", data);
+}
+
+async function uploadFileForBuffer(buffer) {
+  const formData = new FormData();
+  formData.append("buffer", buffer);
+
+  const response = await fetch("http://localhost:3000/upload", {
+    method: "POST",
+    body: formData,
+  });
+  const data = await response.json();
+  console.log("File uploaded by buffer:", data);
+}
+
+async function uploadFileForBase64(base64) {
+  const formData = new FormData();
+  formData.append("base64", base64);
+
+  const response = await fetch("http://localhost:3000/upload", {
+    method: "POST",
+    body: formData,
+  });
+  const data = await response.json();
+  console.log("File uploaded by base64:", data);
+}
+
+// inputFileEl.addEventListener("change", (event) => {
+//   const file = event.target.files[0];
+//   uploadFileForFile(file);
+// });
+
+// inputFileEl.addEventListener("change", async (event) => {
+//   const file = event.target.files[0];
+//   const buffer = new Uint8Array(await file.arrayBuffer());
+//   const blob = new Blob([buffer]);
+//   uploadFileForBlob(blob);
+// });
+
+// inputFileEl.addEventListener("change", async (event) => {
+//   const file = event.target.files[0];
+//   const buffer = new Uint8Array(await file.arrayBuffer());
+//   uploadFileForBuffer(buffer);
+// });
 
 inputFileEl.addEventListener("change", (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+  const file = event.target.files[0];
+  const reader = new FileReader();
 
-    reader.onload = (e)=>{
-        const arrayBuffer = e.target.result;
+  reader.onload = async (event) => {
+    const base64 = event.target.result;
+    uploadFileForBase64(base64);
+  };
 
-        console.log("File readed:", arrayBuffer);
-        console.log("WebSocket 正在发送文件");
-        socket.send(arrayBuffer);
-    }
-
-    reader.onerror = (error) => {
-        console.error("Error reading file:", error);
-    };
-
-    reader.readAsArrayBuffer(file); 
+  reader.readAsDataURL(file);
 });
--- webSocket/server/koa-ws/transferImage/buffer.js
+++ webSocket/server/koa-ws/transferImage/buffer.js
@@ -3,62 +3,68 @@ const WS = require("ws");
 const Koa = require("koa");
 const HTTP = require("http");
 const Path = require("path");
+const KoaBody = require("koa-body");
+const KoaCors = require("@koa/cors");
 const KoaRouter = require("koa-router");
 const KoaStatic = require("koa-static");
 
+const PORT = 3000;
 const app = new Koa();
 const clients = new Set();
+const { koaBody } = KoaBody;
 const router = new KoaRouter();
 const server = HTTP.createServer(app.callback());
 const wss = new WS.Server({ server });
 const staticDir = Path.join(__dirname, "static");
 const uploadDir = Path.join(staticDir, "uploads");
 
-function processUploadBase(data, filename) {
-  const filePath = Path.join(uploadDir, filename);
-  FS.writeFileSync(filePath, data);
-  return "http://localhost:3000/uploads/" + filename;
+if (!FS.existsSync(uploadDir)) {
+  FS.mkdirSync(uploadDir, { recursive: true });
 }
 
-function processUploadForFile(file, filename) {
-  const buffer = FS.readFileSync(file.path);
-  const filenameNew = filename ?? `file_${Date.now()}.png`;
-  const fileUrl = processUploadBase(buffer, filenameNew);
+function processUploadBase(data, filename) {
+  const filenameNew = filename ?? `${Date.now()}.png`;
+  const filePath = Path.join(uploadDir, filenameNew);
+  FS.writeFileSync(filePath, data);
+  return "http://localhost:3000/uploads/" + filenameNew;
+}
+
+function processUploadForFile(file) {
+  // 读取文件内容: 读取的是文件的临时路径，这是在上传过程中生成的，Koa 的 koa-body 中间件会将上传的文件保存到临时目录，并提供这个路径。因此，您可以通过这个路径读取文件。
+  const buffer = FS.readFileSync(file.filepath);
+  const fileUrl = processUploadBase(buffer);
   return fileUrl;
 }
 
-function processUploadForBlob() {}
-
-function processUploadForBase64() {}
-
-function processUploadForBuffer() {}
-
-function getUploadedFileURL(fileName) {
-  return `http://localhost:3000/uploads/${fileName}`;
+function processUploadForBase64(base64) {
+  const base64Data = base64.replace(/^data:([A-Za-z-+/]+);base64,/, "");
+  const buffer = Buffer.from(base64Data, "base64");
+  const fileUrl = processUploadBase(buffer);
+  return fileUrl;
 }
 
-function saveUploadedFile() {}
+function processUploadForBuffer(buffer) {
+  const bufferNew = Buffer.from(buffer, "binary");
+  const fileUrl = processUploadBase(bufferNew);
+  return fileUrl;
+}
 
-function saveUploadedBufferToFile(buffer) {
-  if (!FS.existsSync(uploadDir)) {
-    FS.mkdirSync(uploadDir, { recursive: true });
-  }
-
-  const fileName = `${Date.now()}.png`;
-  const filePath = Path.join(uploadDir, fileName);
-  FS.writeFileSync(filePath, buffer);
-  return filePath;
+function receiveHttpStreamData(req) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    req.on("data", (chunk) => chunks.push(chunk));
+    req.on("end", () => resolve(Buffer.concat(chunks)));
+    req.on("error", (err) => reject(err));
+  });
 }
 
 wss.on("connection", (ws) => {
   clients.add(ws);
 
   ws.on("message", (message) => {
-    const buffer = Buffer.from(message);
-    const savedFilePath = saveUploadedBufferToFile(buffer);
-    const fileURL = getUploadedFileURL(Path.basename(savedFilePath));
-    console.log("fileURL:", fileURL);
-    ws.send(`fileURL: ${savedFilePath}`);
+    const fileUrl = processUploadForBuffer(message);
+    console.log("Websocket URL fileUrl", fileUrl);
+    ws.send(`fileURL: ${fileUrl}`);
   });
 
   ws.on("close", () => {
@@ -66,13 +72,65 @@ wss.on("connection", (ws) => {
   });
 });
 
-router.post("/upload", async () => {
-  const file = ctx.request.files.file;
+router.get("/", (ctx) => {
+  ctx.body = "Hello, Koa!";
 });
 
-app.use(KoaStatic(staticDir));
-app.use();
+router.post("/upload", async (ctx) => {
+  const { files, body } = ctx.request;
+  const contentType = ctx.request.headers["content-type"];
 
-server.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  let fileUrl = null;
+
+  if (files?.file || body?.file) {
+    const file = files?.file || body?.file;
+    fileUrl = processUploadForFile(file);
+  } else if (files?.base64 || body?.base64) {
+    const base64 = files?.base64 || body?.base64;
+    fileUrl = processUploadForBase64(base64);
+  } else if (files?.blob || body?.blob) {
+    const blob = files?.blob || body?.blob;
+    fileUrl = processUploadForFile(blob);
+  } else if (
+    contentType === "application/octet-stream" ||
+    files?.buffer ||
+    body?.buffer
+  ) {
+    const buffer =
+      files?.buffer || body?.buffer || (await receiveHttpStreamData(ctx.req));
+    fileUrl = processUploadForBuffer(buffer);
+  }
+
+  ctx.body = {
+    code: 200,
+    data: fileUrl,
+    message: "File uploaded",
+  };
+});
+
+app.use(
+  KoaCors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(
+  koaBody({
+    text: true,
+    json: true,
+    multipart: true,
+    urlencoded: true,
+    encoding: "gzip",
+    formidable: {
+      uploadDir: uploadDir,
+      keepExtensions: true,
+    },
+  })
+);
+app.use(KoaStatic(staticDir));
+
+app.use(router.routes()).use(router.allowedMethods());
+server.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
 });
