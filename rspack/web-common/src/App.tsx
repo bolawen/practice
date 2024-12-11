4965db1cf452e2f2a8eef79de6690fcfacf280a7
zhangwenqiang <zhangwenqiang@umu.com>
2024-12-11 16:16:23 +0800

    范德萨范德

--- rspack/web-common/src/App.tsx
+++ rspack/web-common/src/App.tsx
@@ -1,7 +1,4 @@
 import "./App.scss";
-import "./App.less";
-import appStyleLess from "./App.module.less";
-import appStyleScss from "./App.module.scss";
 
 function App() {
   const a = 1;
@@ -11,9 +8,9 @@ function App() {
 
   return (
     <div className="app">
-      嘻嘻哈哈
-      <div className={appStyleLess.boxLess}></div>
-      <div className={appStyleScss.boxScss}></div>
+      嘻嘻哈哈发送到；发的酸；发；安抚巾；但是甲方；大家安抚；打撒甲方发了快递舒服了看电视剧斯洛伐克到时间了咖啡店索拉卡敷设电缆发的时刻冷风机洛克菲勒看电视剧路口放大升级卡拉甲方看来是李逵负荆单身快乐发 
+      {/* <div className={appStyleLess.boxLess}></div> */}
+      {/* <div className={appStyleScss.boxScss}></div> */}
     </div>
   );
 }
